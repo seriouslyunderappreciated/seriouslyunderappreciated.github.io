e176a9d101296ba0f903f0984c4f64dce607ae04
seriouslyunderappreciated <42812162+seriouslyunderappreciated@users.noreply.github.com>
2025-12-05 16:20:54 +0000

    Add files via upload

--- scripts/scroll-top.js
+++ scripts/scroll-top.js
@@ -1,14 +1,12 @@
-(function () {
-    var btn = $('#button');
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 300) {
-            btn.addClass('show');
-        } else {
-            btn.removeClass('show');
-        }
-    });
-    btn.on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: 0 }, '300');
-    });
-})();
+        var btn = $('#button');
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, '300');
+        });
